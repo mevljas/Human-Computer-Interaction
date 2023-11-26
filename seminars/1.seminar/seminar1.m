058161d9a542e9f19f9a9f73c357943c90246692
mevljas <sebastjan.mevlja@telekom.si>
2023-11-26 20:06:39 +0100

    set all graphs to the same scale

--- seminars/1.seminar/seminar1.m
+++ seminars/1.seminar/seminar1.m
@@ -1,23 +1,22 @@
 set(gcf, 'Position',  [100, 100, 1500, 800])
 
 prompt = 'Prosimo vnesite številko želenega subjekta: ';
-%subject = input(prompt);
-subject = 1;
+subject = 1;  % Set the subject to the desired value
+
 if isempty(subject)
-   subject = 16; 
+    subject = 16;
 end
+
 if (subject < 10)
-    subject = strcat('0',int2str(subject));
+    subject = strcat('0', int2str(subject));
 else
     subject = int2str(subject);
 end
 
-
-file = strcat('S0',subject,'\S0',subject,'R01.edf');
+file = strcat('S0', subject, '\S0', subject, 'R01.edf');
 disp('Odpiram datoteko: ');
 disp(file);
 [signals, fs, tm] = rdsamp(file, 1:64);
-
 
 % Perform Independent Component Analysis (ICA) on the EEG data
 [icasig, A, W] = fastica(signals', 'approach', 'symm', 'g', 'tanh');
@@ -28,17 +27,18 @@ components_per_tab = 5;  % Adjust this based on your preference
 num_components = size(icasig, 1);
 num_tabs = ceil(num_components / components_per_tab);
 
-% Create a tab group
-tab_group = uitabgroup;
+% Initialize min and max values for y-axis limits across all tabs
+y_min_all_tabs = Inf;
+y_max_all_tabs = -Inf;
 
 % Visualize individual independent components in separate tabs
 for t = 1:num_tabs
     % Create a new tab
-    tab = uitab(tab_group, 'Title', ['Tab ' num2str(t)]);
-    
+    tab = uitab('Title', ['Tab ' num2str(t)]);
+
     % Create axes for the tab
     axes('Parent', tab);
-    
+
     % Plot components in the current tab
     for i = 1:components_per_tab
         index = (t - 1) * components_per_tab + i;
@@ -46,9 +46,24 @@ for t = 1:num_tabs
             subplot(components_per_tab, 1, i);
             plot(tm, icasig(index, :));
             title(['Independent Component ' num2str(index)]);
+
+            % Update y-axis limits across all tabs
+            y_min_all_tabs = min(y_min_all_tabs, min(icasig(index, :)));
+            y_max_all_tabs = max(y_max_all_tabs, max(icasig(index, :)));
         end
     end
 end
+
+% Set the same y-axis limits for all subplots and tabs
+for t = 1:num_tabs
+    for i = 1:components_per_tab
+        tab = findobj(gcf, 'Type', 'uitab', 'Title', ['Tab ' num2str(t)]);
+        axes('Parent', tab);
+        subplot(components_per_tab, 1, i);
+        ylim([y_min_all_tabs, y_max_all_tabs]);
+    end
+end
+
 
 % Identify the independent component(s) corresponding to eye artifacts
 % You may need to visualize the independent components and manually select the ones related to eye artifacts
